d7592553be8fb58e819836928c13fa5f4e35e5fd
TheArabuka <114321028+TheArabuka@users.noreply.github.com>
2022-09-25 13:09:41 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+$array = [
+    [
+        'name' => 'FirstDima',
+        'age' => '16',
+        'stats' => 'general 1'
+    ],
+
+    [
+        'name' => 'SecondDima',
+        'age' => '14',
+        'stats' => 'ThePalpatine'
+    ],
+
+    [
+        'name' => 'Den',
+        'age' => '16',
+        'stats' => 'the funniest developer',
+    ],
+
+    [
+        'name' => 'Sergey',
+        'age' => '16',
+        'stats' => 'general 2',
+    ],
+
+    [
+        'name' => 'I',
+        'age' => '14',
+        'stats' => 'not seriosly',
+    ],
+];
+
+print('<pre>');
+print_r($array);
+print('</pre>');
